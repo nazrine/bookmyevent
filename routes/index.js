e5bf2837f77c7ebb5a31831e79dfc587fc5b4bb7
nazrine <fathimanazrinem@gmail.com>
2026-06-30 12:18:34 +0530

    added contactt page with message post logic

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@ import HomeView from '@/components/HomeView.vue'
 import ManagerDashboard from '@/components/ManagerDashboard.vue'
 import EventDetailsView from '@/components/EventDetailsView.vue'
 import About from '@/components/About.vue'
+import Contact from '@/components/Contact.vue'
 
 const routes = [
     {path:'/', component: HomeView},
     {path:'/manager-dashboard', component: ManagerDashboard},
     {path:'/event-details', component: EventDetailsView},
-    {path:'/about', component: About}
+    {path:'/about', component: About},
+    {path: '/contact', component:Contact}
 ] 
 
 const router = createRouter({
